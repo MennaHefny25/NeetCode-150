3f712e8df17d43b7184aa177e47702e03875b4a1
MennaHefny25 <mennahefny25@gmail.com>
2024-05-16 04:12:07 +0300

    done

---
+++ Valid Anagram/isAnagram.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(list(s)) == sorted(list(t))  
+    
+s = "menna"
+print(list(s))
